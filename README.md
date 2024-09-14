841204d526019127a89aaaef823449bdd6aae808
Vivek Vivek <shukla.vivek1993@gmail.com>
2024-09-14 19:11:26 +0530

    adding changes to the utils and micrograd to handle new vis

---
+++ README.md
@@ -0,0 +1,30 @@
+
+# micrograd
+
+> micrograd is the only piece of code you need to train neural networks. Everything else is just efficiency.
+
+In this module we build a tiny "autograd" engine (short for automatic gradient) that implements the backpropagation algorithm, as it was prominently popularized for training neural networks in the 1986 paper [Learning Internal Representations by Error Propagation](https://stanford.edu/~jlmcc/papers/PDP/Volume%201/Chap8_PDP86.pdf) by Rumelhart, Hinton and Williams. This repository builds on the earlier repo [karpathy/micrograd](https://github.com/karpathy/micrograd), but modifies into into an LLM101n module.
+
+The code we build here is the heart of neural network training - it allows us to calculate how we should update the parameters of a neural network in order to make it better at some task, such as the one of next token prediction in autoregressive language models. This exact same algorithm is used in all modern deep learning libraries, such as PyTorch, TensorFlow, JAX, and others, except that those libraries are much more optimized and feature-rich.
+
+!WIP!
+
+Very early draft, putting up a possible first version of what I have in mind. In simple terms, 3-way classification for 2D training data. Very visual, easy to understand, good for intuition building.
+
+Covers:
+- Autograd engine (micrograd)
+- Neural network (NN) with 1 hidden layer (MLP) built on top of it
+- Training loop: loss function, backpropagation, parameter updates
+
+**Interactive Demo**. There is a nice visaulization of the "action" during training, showing the exact computational graph. The full computational graph includes the entire dataset which is too much. So instead, here we pick a single datapoint to forward through the network and show that graph alone. In this case we're forwarding the origin (0,0) and a fictional label 0, running the forward pass and the loss, doing backward, and then showing the data/grads. The process to get this visualization working:
+
+1. First run `python micrograd.py` and make sure it saves the `graph.svg`, which is the connectivity graph. You'll need to install graphviz if you don't have it. E.g. on my MacBook this is `brew install graphviz` followed by `pip install graphviz`.
+2. Once we have the `graph.svg` file, we load it from the HTML. Because of cross-origin security issues when loading the svg from the file system, we can't just open up the HTML page directly and need to serve these files. The easiest way is to run a simple python webserver with `python -m http.server`. Then open up the localhost URL it gives you, and open the `micrograd.html` page. You'll see a really cool visualization of the training process yay!
+
+TODOs:
+- Parallel implementation in C that prints the same thing
+- Many improvements to the interactive web demo. Should be able to step through the optimization in more detail, select the "fictional" example to forward through the graph, possibly add/delete datapoints and see how the network responds to it, etc. Probably the entire visualization can be made significantly nicer.
+
+### License
+
+MIT
---
+++ micrograd.py
@@ -0,0 +1,303 @@
+"""
+Defines a simple autograd engine and uses it to classify points in the plane
+to 3 classes (red, green, blue) using a simple multilayer perceptron (MLP).
+"""
+import math
+from utils import RNG, gen_data_yinyang, draw_dot, vis_color
+random = RNG(42)
+
+# -----------------------------------------------------------------------------
+# Value. Similar to PyTorch's Tensor but only of size 1 element
+
+class Value:
+    """ stores a single scalar value and its gradient """
+
+    def __init__(self, data, _prev=(), _op=''):
+        self.data = data
+        self.grad = 0
+        # internal variables used for autograd graph construction
+        self._backward = lambda: None
+        self._prev = _prev
+        self._op = _op # the op that produced this node, for graphviz / debugging / etc
+
+    def __add__(self, other):
+        other = other if isinstance(other, Value) else Value(other)
+        out = Value(self.data + other.data, (self, other), '+')
+
+        def _backward():
+            self.grad += out.grad
+            other.grad += out.grad
+        out._backward = _backward
+
+        return out
+
+    def __mul__(self, other):
+        other = other if isinstance(other, Value) else Value(other)
+        out = Value(self.data * other.data, (self, other), '*')
+
+        def _backward():
+            self.grad += other.data * out.grad
+            other.grad += self.data * out.grad
+        out._backward = _backward
+
+        return out
+
+    def __pow__(self, other):
+        assert isinstance(other, (int, float)), "only supporting int/float powers for now"
+        out = Value(self.data**other, (self,), f'**{other}')
+
+        def _backward():
+            self.grad += (other * self.data**(other-1)) * out.grad
+        out._backward = _backward
+
+        return out
+
+    def relu(self):
+        out = Value(0 if self.data < 0 else self.data, (self,), 'ReLU')
+
+        def _backward():
+            self.grad += (out.data > 0) * out.grad
+        out._backward = _backward
+
+        return out
+
+    def tanh(self):
+        out = Value(math.tanh(self.data), (self,), 'tanh')
+
+        def _backward():
+            self.grad += (1 - out.data**2) * out.grad
+        out._backward = _backward
+
+        return out
+
+    def exp(self):
+        out = Value(math.exp(self.data), (self,), 'exp')
+
+        def _backward():
+            self.grad += out.data * out.grad
+        out._backward = _backward
+
+        return out
+
+    def log(self):
+        # (this is the natural log)
+        out = Value(math.log(self.data), (self,), 'log')
+
+        def _backward():
+            self.grad += (1/self.data) * out.grad
+        out._backward = _backward
+
+        return out
+
+    def backward(self):
+
+        # topological order all of the children in the graph
+        topo = []
+        visited = set()
+        def build_topo(v):
+            if v not in visited:
+                visited.add(v)
+                for child in v._prev:
+                    build_topo(child)
+                topo.append(v)
+        build_topo(self)
+
+        # go one variable at a time and apply the chain rule to get its gradient
+        self.grad = 1
+        for v in reversed(topo):
+            v._backward()
+
+    def __neg__(self): # -self
+        return self * -1.0
+
+    def __radd__(self, other): # other + self
+        return self + other
+
+    def __sub__(self, other): # self - other
+        return self + (-other)
+
+    def __rsub__(self, other): # other - self
+        return other + (-self)
+
+    def __rmul__(self, other): # other * self
+        return self * other
+
+    def __truediv__(self, other): # self / other
+        return self * other**-1
+
+    def __rtruediv__(self, other): # other / self
+        return other * self**-1
+
+    def __repr__(self):
+        return f"Value(data={self.data}, grad={self.grad})"
+
+# -----------------------------------------------------------------------------
+# Multi-Layer Perceptron (MLP) network. Module here is similar to PyTorch's nn.Module
+
+class Module:
+
+    def zero_grad(self):
+        for p in self.parameters():
+            p.grad = 0
+
+    def parameters(self):
+        return []
+
+class Neuron(Module):
+
+    def __init__(self, nin, nonlin=True):
+        self.w = [Value(random.uniform(-1, 1) * nin**-0.5) for _ in range(nin)]
+        self.b = Value(0)
+        self.nonlin = nonlin
+        # color the neuron params light green (only used in graphviz visualization)
+        vis_color(self.w, "lightgreen")
+        vis_color([self.b], "blue")
+
+    def __call__(self, x):
+        act = sum((wi*xi for wi,xi in zip(self.w, x)),self.b)
+        return act.tanh() if self.nonlin else act
+
+    def parameters(self):
+        return self.w + [self.b]
+
+    def __repr__(self):
+        return f"{'TanH' if self.nonlin else 'Linear'}Neuron({len(self.w)})"
+
+class Layer(Module):
+
+    def __init__(self, nin, nout, **kwargs):
+        self.neurons = [Neuron(nin, **kwargs) for _ in range(nout)]
+
+    def __call__(self, x):
+        out = [n(x) for n in self.neurons]
+        return out
+
+    def parameters(self):
+        return [p for n in self.neurons for p in n.parameters()]
+
+    def __repr__(self):
+        return f"Layer of [{', '.join(str(n) for n in self.neurons)}]"
+
+class MLP(Module):
+
+    def __init__(self, nin, nouts):
+        sz = [nin] + nouts
+        self.layers = [Layer(sz[i], sz[i+1], nonlin=i!=len(nouts)-1) for i in range(len(nouts))]
+
+    def __call__(self, x):
+        for layer in self.layers:
+            x = layer(x)
+        return x
+
+    def parameters(self):
+        return [p for layer in self.layers for p in layer.parameters()]
+
+    def __repr__(self):
+        return f"MLP of [{', '.join(str(layer) for layer in self.layers)}]"
+
+# -----------------------------------------------------------------------------
+# loss function: the negative log likelihood (NLL) loss
+# NLL loss = CrossEntropy loss when the targets are one-hot vectors
+# same as PyTorch's F.cross_entropy
+
+def cross_entropy(logits, target):
+    # subtract the max for numerical stability (avoids overflow)
+    # commenting these two lines out to get a cleaner visualization
+    # max_val = max(val.data for val in logits)
+    # logits = [val - max_val for val in logits]
+    # 1) evaluate elementwise e^x
+    ex = [x.exp() for x in logits]
+    # 2) compute the sum of the above
+    denom = sum(ex)
+    # 3) normalize by the sum to get probabilities
+    probs = [x / denom for x in ex]
+    # 4) log the probabilities at target
+    logp = (probs[target]).log()
+    # 5) the negative log likelihood loss (invert so we get a loss - lower is better)
+    nll = -logp
+
+    return nll
+
+# -----------------------------------------------------------------------------
+# The AdamW optimizer, same as PyTorch optim.AdamW
+
+class AdamW:
+    def __init__(self, parameters, lr=1e-1, betas=(0.9, 0.95), eps=1e-8, weight_decay=0.0):
+        self.parameters = parameters
+        self.lr = lr
+        self.beta1, self.beta2 = betas
+        self.eps = eps
+        self.weight_decay = weight_decay
+        # state of the optimizer
+        self.t = 0 # step counter
+        for p in self.parameters:
+            p.m = 0 # first moment
+            p.v = 0 # second moment
+
+    def step(self):
+        self.t += 1
+        for p in self.parameters:
+            if p.grad is None:
+                continue
+            p.m = self.beta1 * p.m + (1 - self.beta1) * p.grad
+            p.v = self.beta2 * p.v + (1 - self.beta2) * (p.grad ** 2)
+            m_hat = p.m / (1 - self.beta1 ** self.t)
+            v_hat = p.v / (1 - self.beta2 ** self.t)
+            p.data -= self.lr * (m_hat / (v_hat ** 0.5 + 1e-8) + self.weight_decay * p.data)
+
+    def zero_grad(self):
+        for p in self.parameters:
+            p.grad = 0
+
+# -----------------------------------------------------------------------------
+# let's train!
+
+# generate a dataset with 100 2-dimensional datapoints in 3 classes
+train_split, val_split, test_split = gen_data_yinyang(random, n=100)
+
+# init the model: 2D inputs, 8 neurons, 3 outputs (logits)
+model = MLP(2, [8, 3])
+
+# optimize using AdamW
+optimizer = AdamW(model.parameters(), lr=1e-1, weight_decay=1e-4)
+
+def loss_fun(model, split):
+    # evaluate the loss function on a given data split
+    total_loss = Value(0.0)
+    for x, y in split:
+        logits = model(x)
+        loss = cross_entropy(logits, y)
+        total_loss = total_loss + loss
+    mean_loss = total_loss * (1.0 / len(split))
+    return mean_loss
+
+# train the network
+num_steps = 100
+for step in range(num_steps):
+
+    # evaluate the validation split every few steps
+    if step % 10 == 0:
+        val_loss = loss_fun(model, val_split)
+        print(f"step {step+1}/{num_steps}, val loss {val_loss.data:.6f}")
+
+    # forward the network and the loss and all training datapoints
+    loss = loss_fun(model, train_split)
+    # backward pass (calculate the gradient of the loss w.r.t. the model parameters)
+    loss.backward()
+    # update model parameters
+    optimizer.step()
+    optimizer.zero_grad()
+    # print some stats
+    print(f"step {step+1}/{num_steps}, train loss {loss.data}")
+
+# (optional) visualization at the end: take origin (0,0) and draw the computational graph
+x, y = (Value(0.0), Value(0.0)), 0
+loss = loss_fun(model, [(x, y)])
+loss.backward()
+try:
+    vis_color(x, "lightblue") # color the inputs light blue in the visualization of inputs
+    vis_color(loss, "grey91") # color the inputs light grey in the visualization of loss
+    draw_dot(loss)
+
+except Exception as e:
+    print("graphviz not installed? skipped visualization")
